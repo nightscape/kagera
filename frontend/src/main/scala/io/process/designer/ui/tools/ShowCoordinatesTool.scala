6df65431c707c028ccd962a5765731f65481dbf7
Merlijn van Ittersum <vanittersum@gmail.com>
2015-05-17 19:13:35 +0200

    Require explicit tool state type

--- frontend/src/main/scala/io/process/designer/ui/tools/ShowCoordinatesTool.scala
+++ frontend/src/main/scala/io/process/designer/ui/tools/ShowCoordinatesTool.scala
@@ -3,13 +3,13 @@ package io.process.designer.ui.tools
 import io.process.draw._
 import io.process.designer.ui._
 
-class ShowCoordinatesTool extends MouseTool[Drawing] {
+class ShowCoordinatesTool[T] extends MouseTool[T, Drawing] {
 
   def fillStyle = "black"
 
-  override def onMouseEvent = state ⇒ {
+  override def onMouseEvent = (model, state) ⇒ {
     case MouseEvent(t, button, p) ⇒
       val pr = p.round
-      Fill(fillStyle, Text(s"${pr.x},${pr.y}", p))
+      (model, Fill(fillStyle, Text(s"${pr.x},${pr.y}", p)))
   }
 }
