cb925ff5af1c07fd78372a89e5efa3587cfb19b5
Merlijn van Ittersum <vanittersum@gmail.com>
2015-11-29 17:28:51 +0100

    Generalize scala graph code in coloured package for any kind of bi partite graph

--- statebox/src/main/scala/io/process/statebox/common/Settings.scala
+++ statebox/src/main/scala/io/process/statebox/common/Settings.scala
@@ -4,7 +4,7 @@ import com.typesafe.config.{ ConfigFactory, Config }
 
 class Settings(config: Config) {
 
-  val http = new {
+  object http {
     val interface = config.getString("http.interface")
     val port = config.getInt("http.port")
   }
