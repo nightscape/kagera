e2c675fdd82a42869ca9da51889a79e23844e95f
Merlijn van Ittersum <merlijn.van.ittersum@ing.com>
2016-05-02 15:47:19 +0200

    Seperate tokengame into it's own file, use trait instead of type alias for PTProcess

--- api/src/main/scala/io/kagera/api/package.scala
+++ api/src/main/scala/io/kagera/api/package.scala
@@ -32,7 +32,7 @@ package object api {
    * @tparam T The transition type
    * @tparam M The marking type
    */
-  type PTProcess[P, T, M] = PetriNet[P, T] with TokenGame[P, T, M] with TransitionExecutor[P, T, M]
+  trait PTProcess[P, T, M] extends PetriNet[P, T] with TokenGame[P, T, M] with TransitionExecutor[P, T, M]
 
   /**
    * Type class for marking 'like' semantics.
@@ -80,23 +80,6 @@ package object api {
     this: PetriNet[P, T] ⇒
 
     def fireTransition(marking: M)(transition: T): Future[M]
-  }
-
-  trait TokenGame[P, T, M] {
-
-    this: PetriNet[P, T] ⇒
-
-    def enabledParameters(m: M): Map[T, Iterable[M]] = {
-      // inefficient, fix
-      enabledTransitions(m).view.map(t ⇒ t -> consumableMarkings(m)(t)).toMap
-    }
-
-    def consumableMarkings(m: M)(t: T): Iterable[M]
-
-    // horribly inefficient, fix
-    def isEnabled(marking: M)(t: T): Boolean = enabledTransitions(marking).contains(t)
-
-    def enabledTransitions(marking: M): Set[T]
   }
 }
 
