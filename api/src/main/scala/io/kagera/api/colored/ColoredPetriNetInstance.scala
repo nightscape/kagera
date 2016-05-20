61a7877510942802c13a8a20d8762eb7e09f6a96
Merlijn van Ittersum <merlijn.van.ittersum@ing.com>
2016-05-20 13:21:30 +0200

    bugfix accumuluated marking building in ColoredPetriNetInstance

--- api/src/main/scala/io/kagera/api/colored/ColoredPetriNetInstance.scala
+++ api/src/main/scala/io/kagera/api/colored/ColoredPetriNetInstance.scala
@@ -18,20 +18,23 @@ class ColoredPetriNetInstance(process: PetriNetProcess[Place, Transition, Colore
   override def accumulatedMarking: ColoredMarking = accumulated
 
   override def fireTransition(t: Transition, data: Option[Any]): Future[ColoredMarking] = {
-    process.fireTransition(currentMarking)(t, data).flatMap(stepManagedRecursive).map { newMarking ⇒
+    process.fireTransition(currentMarking)(t, data).map(applyChange(t)).flatMap(stepManagedRecursive)
+  }
 
-      val newTokens = newMarking remove currentMarking
-      accumulated = accumulatedMarking produce newTokens
-      currentMarking = newMarking
-      newMarking
-    }
+  def applyChange(t: Transition)(newMarking: ColoredMarking): ColoredMarking = {
+    val newTokens = newMarking remove currentMarking
+    println(s"$t fired, produced: $newTokens")
+    accumulated = accumulated produce newTokens
+    println(s"accumulated marking: $accumulated")
+    currentMarking = newMarking
+    newMarking
   }
 
   // this is very dangerous hack, could go into an infinite recursive loop
   def stepManagedRecursive(marking: ColoredMarking): Future[ColoredMarking] = {
-    process.enabledTransitions(marking).find(_.isManaged).map(t ⇒
-      process.fireTransition(marking)(t, None).flatMap(stepManagedRecursive)
-    ).getOrElse(Future.successful(marking))
+    process.enabledTransitions(marking).find(_.isManaged).map { t ⇒
+      process.fireTransition(marking)(t, None).map(applyChange(t)).flatMap(stepManagedRecursive)
+    }.getOrElse(Future.successful(marking))
   }
 
   override def step(): Future[ColoredMarking] = ???
