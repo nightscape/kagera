e95655eee2a590310ae09408d34d2faec68a0e27
Erwin (E.) Rooijakkers <erwin.rooijakkers@ing.nl>
2016-06-01 10:29:00 +0200

    Append id to differentiate in visualization

--- api/src/main/scala/io/kagera/api/colored/AbstractTransition.scala
+++ api/src/main/scala/io/kagera/api/colored/AbstractTransition.scala
@@ -8,7 +8,7 @@ abstract class AbstractTransition[I, O](override val id: Long,
   override val maximumOperationTime: Duration = Duration.Undefined)
     extends Transition {
 
-  override def toString = label
+  override def toString = label + "-" + id // Append ID to differentiate places/transition with the same name in visualization
 
   type Input = I
   type Output = O
