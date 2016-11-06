ece86adc8b0ee067112420c6bef2741d9f35d273
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2016-11-06 10:20:30 +0100

    akka; add optional string manifest to serialized objects, small cleanups

--- api/src/main/scala/io/kagera/api/colored/TransitionExecutor.scala
+++ api/src/main/scala/io/kagera/api/colored/TransitionExecutor.scala
@@ -7,27 +7,27 @@ import scala.concurrent.{ ExecutionContext, Future }
 
 trait TransitionExecutor[S] {
 
-  this: PetriNet[Place[_], Transition[_, _, _]] with TokenGame[Place[_], Transition[_, _, _], Marking] ⇒
+  this: PetriNet[Place[_], Transition[_, _, _]] ⇒
 
-  val transitionFunctions: Map[Transition[_, _, _], _] =
-    transitions.map(t ⇒ t -> t.apply(inMarking(t), outMarking(t))).toMap
+  val transitionFunctions: Map[Transition[_, _, _], _] = transitions.map(t ⇒ t -> t.apply(inMarking(t), outMarking(t))).toMap
 
   def tfn[Input, Output](t: Transition[Input, Output, S]): (Marking, S, Input) ⇒ Task[(Marking, Output)] =
     transitionFunctions(t).asInstanceOf[(Marking, S, Input) ⇒ Task[(Marking, Output)]]
 
-  def fireTransition[Input, Output](t: Transition[Input, Output, S])(consume: Marking, state: S, input: Input): Task[(Marking, Output)] = {
+  def fireTransition[Input, Output](t: Transition[Input, Output, S]): (Marking, S, Input) ⇒ Task[(Marking, Output)] = {
+    (consume, state, input) ⇒
 
-    if (consume.multiplicities != inMarking(t)) {
-      // TODO make more explicit what is wrong here, mention the first multiplicity that is incorrect.
-      Task.fail(new IllegalArgumentException(s"Transition $t may not consume $consume"))
-    }
+      def handleFailure: PartialFunction[Throwable, Task[(Marking, Output)]] = {
+        case e: Throwable ⇒ Task.fail(new TransitionFailedException(t, e))
+      }
 
-    def handleFailure: PartialFunction[Throwable, Task[(Marking, Output)]] = {
-      case e: Throwable ⇒ Task.fail(new TransitionFailedException(t, e))
-    }
+      if (consume.multiplicities != inMarking(t)) {
+        // TODO make more explicit what is wrong here, mention the first multiplicity that is incorrect.
+        Task.fail(new IllegalArgumentException(s"Transition $t may not consume $consume"))
+      }
 
-    try {
-      tfn(t)(consume, state, input).handleWith { handleFailure }
-    } catch { handleFailure }
+      try {
+        tfn(t)(consume, state, input).handleWith { handleFailure }
+      } catch { handleFailure }
   }
 }
